d3fa2ef8142f813cb932f83674ba59d01ee43b5b
Aleks247 <aleks247bg@gmail.com>
2026-03-14 15:52:51 +0200

    Add user DTOs and mapper
    Introduce SimpleUserDTO and UserResponseDTO to represent user payloads, using Lombok for boilerplate. Add UserMapper with toDTO and toSimpleDTO to map fields from the User entity to the new DTOs, preparing the API layer for user data transfer.

---
+++ src/main/java/e_commerce/clothing_brand/dto/user/SimpleUserDTO.java
@@ -0,0 +1,13 @@
+package e_commerce.clothing_brand.dto.user;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SimpleUserDTO {
+    private Long id;
+    private String username;
+}
