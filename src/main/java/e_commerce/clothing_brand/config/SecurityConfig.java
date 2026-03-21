2e9ded3797dd813211303c014e45dba9b2390736
Aleksandur Dimitrov <aleks247bg@gmail.com>
2026-03-21 13:36:45 +0200

    Updating security config and adding the JWT dependencies

--- src/main/java/e_commerce/clothing_brand/config/SecurityConfig.java
+++ src/main/java/e_commerce/clothing_brand/config/SecurityConfig.java
@@ -2,20 +2,29 @@ package e_commerce.clothing_brand.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/products/**").permitAll()
+//                        .requestMatchers("/users/**").hasRole("ADMIN")
+//                        .requestMatchers("/orders/**").hasRole("ADMIN")
+//                        .anyRequest().authenticated()
                         .anyRequest().permitAll()
-                );
+                )
+                .httpBasic(Customizer.withDefaults()) // Enable Basic Auth (for testing/APIs)
+                .formLogin(Customizer.withDefaults());
+
 
         return http.build();
     }
